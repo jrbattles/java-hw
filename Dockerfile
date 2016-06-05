67ca745b4b3683601f862d188ea064e4352926cb
jrbattles <jason.r.battles@gmail.com>
2016-06-04 19:11:38 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM java:7
+copy JavaHelloWorld.java .
+RUN javac JavaHelloWorld.java
+
+CMD ("java", "JavaHelloWorld")
---
+++ JavaHelloWorld.java
@@ -0,0 +1,8 @@
+public class JavaHelloWorld
+{
+    public static voice main (String [] args)
+    {
+        System.out.printIn("Java Hello World");
+        System.out.printIn("Hello again");
+    }
+}
